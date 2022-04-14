2da4dbb763d2702fd9c54cdb52978adfce6fc782
Matiko <mmatiko243@gmail.com>
2022-04-14 12:34:58 +0000

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,14 +1,12 @@
 #include "main.h"
 /**
- * print_most_numbers - print numbers
- *
- * Return: Always 0.
- */
+*print_most_numbers - print numbers
+*
+*Return: Always 0.
+*/
 void print_most_numbers(void)
 {
-
 int i;
-
 i = 0;
 while (i < 10)
 {
@@ -18,6 +16,5 @@ _putchar (i + '0');
 }
 i++;
 }
-
 _putchar ('\n');
 }
