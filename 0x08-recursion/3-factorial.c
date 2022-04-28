2766583f5a8056dbd627b85a16b8ad85ba9a4e22
Matiko <mmatiko243@gmail.com>
2022-04-28 09:24:46 +0000

    Factorial

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -7,9 +7,9 @@
 int factorial(int n)
 {
 if (n == 0)
-return (1); //first base condition
+return (1); /*first base condition*/
 else if (n < 0)
-return (-1); //second base condition
-else 
-return (n * factorial(n - 1)); //the recursive call
+return (-1); /*second base condition*/
+else
+return (n * factorial(n - 1)); /*the recursive call*/
 }
