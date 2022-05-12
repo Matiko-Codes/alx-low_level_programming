39435e9b5f50f4c5f4e49d01aa034dd7b26e3ece
Matiko <mmatiko243@gmail.com>
2022-05-12 17:16:14 +0000

    Free dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -35,32 +35,38 @@ return (ptr);
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-dog_t *kuchi;
+dog_t *snoopie;
 char *new_name, *new_owner;
+
 if (name == NULL || owner == NULL)
 {
 return (NULL);
 }
-kuchi = malloc(sizeof(dog_t));
-if (kuchi == NULL)
+
+snoopie = malloc(sizeof(dog_t));
+if (snoopie == NULL)
 {
 return (NULL);
 }
+
 new_name = _copy(name);
 if (new_name == NULL)
 {
-free(kuchi);
+free(snoopie);
 return (NULL);
 }
-(*kuchi).name = new_name;
-(*kuchi).age = age;
+(*snoopie).name = new_name;
+
+(*snoopie).age = age;
+
 new_owner = _copy(owner);
 if (new_owner == NULL)
 {
-free((*kuchi).name);
-free(kuchi);
+free((*snoopie).name);
+free(snoopie);
 return (NULL);
 }
-(*kuchi).owner = new_owner;
-return (kuchi);
+#(*snoopie).owner = new_owner;
+
+return (snoopie);
 }
