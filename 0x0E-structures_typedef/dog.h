143749ebbdacf618702b454e259670b6a1201096
Matiko <mmatiko243@gmail.com>
2022-05-12 11:46:46 +0000

    doggo

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,24 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - structure for dog
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
+ * Description: This is dog struct
+ */
+
 struct dog 
 {
 char *name;
 float age;
 char *owner;
 };
+
+typedef struct dog doggo;
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+doggo *new_dog(char *name, float age, char *owner);
+void free_dog(doggo *d);
+
+#endif
