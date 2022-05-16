8bc01450eae1a6564b2c0e6d819d4bdb19370fd7
Matiko <mmatiko243@gmail.com>
2022-05-16 18:03:59 +0000

    index.c

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,4 +1,5 @@
 #include "function_pointers.h"
+
 /**
  * int_index - earches for an integer
  * @array: array to search in
@@ -11,14 +12,16 @@
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
-int i;
-if (array && cmp)
-{
-for (i = 0; i < size; i++)
-{
-if (cmp(array[i]) != 0)
-return (i);
-}
-}
-return (-1);
+	int i;
+
+	if (array && cmp)
+	{
+		for (i = 0; i < size; i++)
+		{
+			if (cmp(array[i]) != 0)
+				return (i);
+		}
+	}
+
+	return (-1);
 }
