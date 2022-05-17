0c6022fea87e05c04bf1c1eeec691b32bb232c93
Matiko <mmatiko243@gmail.com>
2022-05-17 20:06:00 +0000

    Sum all

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -8,13 +8,13 @@ int sum_them_all(const unsigned int n, ...)
 {
 va_list varlist;
 int sum = 0;
-unsigned int k;
+unsigned int i;
 
 if (n == 0)
 return (0);
 
-va_start(varlist, n);
-for (k = 0; k < n; k++)
+va_start(varlist, i);
+for (i = 0; i < n; i++)
 sum += va_arg(varlist, int);
 
 va_end(varlist);
