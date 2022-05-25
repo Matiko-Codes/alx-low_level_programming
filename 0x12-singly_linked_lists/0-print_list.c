b79b592e2e2cc63ac817c918fbec30bc410a8f18
Matiko <mmatiko243@gmail.com>
2022-05-25 13:22:31 +0000

    [0]

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -11,7 +11,7 @@ nelement = 0;
 while (h != NULL)
 {
 if (h->str == NULL)
-printf("[%d] %s\n", "[0]", "(nil)");
+printf("[%d] %s\n", 0, "(nil)");
 else
 printf("[%d] %s\n", h->len, h->str);
 h = h->next;
