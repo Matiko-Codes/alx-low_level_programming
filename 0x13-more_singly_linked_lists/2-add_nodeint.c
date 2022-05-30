472fcd173d763a69233a31b510c02baf8df60abc
Matiko <mmatiko243@gmail.com>
2022-05-30 04:19:22 +0000

    Task 2

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -8,7 +8,7 @@
 listint_t *add_nodeint(listint_t **head, const int n)
 {
 listint_t *latest;
-new = malloc(sizeof(listint_t));
+latest = malloc(sizeof(listint_t));
 if (!latest)
 return (NULL);
 latest->n = n;
