24d3eb32a47a3ff8b23468a20220e8987cd77fd3
Matiko <mmatiko243@gmail.com>
2022-07-13 07:17:29 +0000

    c files

---
+++ 0x18-dynamic_libraries/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strlen - return the length of string
+ * @s: string pointer
+ * Return: nothing
+ */
+int _strlen(char *s)
+{
+int i = 0;
+while (*s++)
+i++;
+return (i);
+}
